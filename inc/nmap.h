ad8588d094d4e48184c17e4a573a6c6bb845e0ba
eedy <eedy@student.42.fr>
2024-11-04 14:17:41 +0100

    add file.c

--- inc/nmap.h
+++ inc/nmap.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "./thread.h"
+#include "thread.h"
+#include "error.h"
+#include "file.h"
+#include "handle_arg.h"
+#include "network.h"
 
 #endif
